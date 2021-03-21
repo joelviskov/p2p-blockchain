e9b7b673810b0b0430c62f67b9c5e7c9b9b40c73
Joel Viskov <joelviskov@gmail.com>
2021-03-21 23:20:01 +0200

    Added ledger component, moved from http library to axios.

--- app.js
+++ app.js
@@ -4,63 +4,125 @@ const host = require('ip').address()
 const address = `${host}:${port}`
 module.exports = { address, port }
 
-const express = require('express')
-const http = require('http')
-const { readPeers, tryAppendPeers, removePeer } = require('./storage')
+const crypto = require('crypto');
+const { getPeers, tryAppendPeers, removePeer, getLedger, tryAppendToLedger } = require('./storage')
 
+const express = require('express')
 const app = express()
+app.use(express.json())
+
+const axios = require('axios')
+axios.defaults.headers.get['Accept'] = 'application/json'
+axios.defaults.headers.post['Accept'] = 'application/json'
 
 app.listen(port, () => {
-  console.log(`Started listening on ${address}`)
+  console.log(`\r\nStarted listening on ${address}`)
 })
 
 app.get('/health', async (req, res) => res.send())
 
 app.get('/get-peers', async (req, res) => {
-  tryAppendPeers([req.query.callback])
-  res.send(await readPeers())
+  await tryAppendPeers([req.query.ip])
+  const savedPeers = await getPeers()
+  res.send(savedPeers)
 })
 
-async function fetchPeers(endpoint) {
-  return new Promise((resolve, reject) => {
-    http.get(`http://${endpoint}/get-peers?callback=${address}`, (res) => {
-      var body = [];
-      res.on('data', (chunk) => {
-        body.push(chunk);
-      });
+app.get('/get-blocks', async (req, res) => {
+  const blocks = await getLedger()
+  res.send(blocks)
+})
 
-      res.on('end', () => {
-        try {
-          resolve(JSON.parse(Buffer.concat(body).toString()));
-        } catch (e) {
-          reject(e);
-        }
-      });
-    }).on('error', () => onPeerError(endpoint))
-  })
+app.get('/get-blocks/:hash', async (req, res) => {
+  const blocks = await getLedger(req.params['hash'])
+  res.send(blocks)
+})
+
+app.get('/get-block/:hash', async (req, res) => {
+  const blocks = await getLedger(hash)
+  const block = blocks.find(x => x.hash == req.params['hash'])
+  res.send(block)
+})
+
+app.post('/transaction', async (req, res) => {
+  console.log(`\r\nReceived transaction.\r\n${JSON.stringify(req.body)}`)
+  const wasSuccess = await distributeTransaction(req.body)
+  if (wasSuccess) {
+    res.send('Added and distributed.')
+    return
+  }
+  res.send("Ignored.")
+})
+
+async function distributeTransaction(transaction) {
+  // Try adding to yourself and if it's new, broadcast to all.
+  const wasSuccess = await tryAppendToLedger([transaction])
+  if (wasSuccess) {
+    for (const node of await getPeers()) {
+      console.log(`\r\nDistributing transaction ${transaction.hash} to ${node}.`)
+      axios.post(`http://${node}/transaction`, transaction)
+        .catch((err) => onPeerError(node, err))
+    }
+  }
+  return wasSuccess
 }
 
 // Fetch unknown connected peers on X interval.
 setInterval(async () => {
-  const knownPeers = await readPeers()
-  for (const endpoint of knownPeers) {
-    fetchPeers(endpoint)
-      .then(res => {
-        tryAppendPeers(res)
+  for (const node of await getPeers()) {
+    axios.get(`http://${node}/get-peers?ip=${address}`)
+      .then(async response => {
+        const healthyNodes = []
+        for (const newNode of response.data) {
+          await axios.get(`http://${newNode}/health`)
+            .then(() => healthyNodes.push(newNode))
+        }
+        await tryAppendPeers(healthyNodes)
       })
-  }
-}, 10000);
-
-// Perform healthcheck and remove unavailable peers.
-setInterval(async () => {
-  const knownPeers = await readPeers()
-  for (const endpoint of knownPeers) {
-    http.get(`http://${endpoint}/health`)
-      .on('error', () => onPeerError(endpoint))
+      .catch((err) => onPeerError(node, err))
   }
 }, 5000);
 
-function onPeerError(endpoint) {
-  console.log(`Error while fetching peers from ${endpoint}. Removing...`)
+// Fetch full ledger on X interval.
+setInterval(async () => {
+  for (const node of await getPeers()) {
+    const url = await getLedgerEndpoint(node)
+    await axios.get(url)
+      .then(async (response) => await tryAppendToLedger(response.data))
+      .catch((err) => onPeerError(node, err))
+  }
+}, 5000);
+
+async function getLedgerEndpoint(node) {
+  const ledger = await getLedger()
+  let base = `http://${node}/get-blocks`
+  if (ledger.length) {
+    const lastHash = ledger.slice(-1)[0].hash
+    base += `/${lastHash}`
+  }
+  return base
+}
+
+function createTransaction() {
+  const payload = { timestamp: Date.now() }
+  const transaction = {
+    hash: crypto.createHash('sha256').update(JSON.stringify(payload)).digest('hex'),
+    data: payload,
+  }
+  console.log(`\r\nCreated transaction:\r\n${JSON.stringify(transaction)}`)
+  distributeTransaction(transaction)
+}
+
+// Create a new transaction on random interval, to simulate real-world.
+(function loop() {
+  var rand = Math.round(Math.random() * 100000) + 10000
+  setTimeout(function () {
+    createTransaction()
+    loop();
+  }, rand);
+}());
+
+function onPeerError(endpoint, error) {
+  console.error(`\r\nLost connection to ${endpoint}. Removing...`)
+  console.error(error)
   removePeer(endpoint)
 }
--- storage.js
+++ storage.js
@@ -2,49 +2,90 @@ const readline = require('readline');
 const fs = require('fs')
 const { port, address } = require('./app')
 
-const file = `connections/peers-${port}.txt`
+// Contains peers of {host}:{port}
+const peerFile = `connections/peers-${port}.txt`
 
-async function readPeers() {
-  if (!fs.existsSync('connections')){
-    fs.mkdirSync('connections');
-  }
+// Contains blocks of { hash, timestamp }
+const blockFile = `blocks/blocks-${port}.txt`
 
-  const fileStream = fs.createReadStream(file, { flags: 'a+' });
-  const lines = readline.createInterface({ input: fileStream, crlfDelay: Infinity });
-
-  const peers = []
-  for await (const peer of lines) {
-    peers.push(peer)
-  }
-
+async function getPeers() {
+  const peers = await readLines(peerFile)
   return peers
 }
 
 function removePeer(peer) {
-  fs.readFile(file, { encoding: 'utf8', flag: 'r+' }, (err, data) => {
-    if (err) throw err
+  fs.readFile(peerFile, { encoding: 'utf8', flag: 'r+' }, (err, data) => {
+    if (err) {
+      console.error(err.message)
+      return
+    }
     const lines = data.split('\r\n')
     const newData = lines.filter(val => val !== peer).join('\r\n')
-    fs.writeFile(file, newData, (err) => {
-      if (err) throw err
+    fs.writeFile(peerFile, newData, (err) => {
+      if (err) {
+        console.error(err.message)
+        return
+      }
     })
   })
 }
 
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index;
-}
-
 async function tryAppendPeers(proposed) {
-  if (!proposed.length) return
+  const knownPeers = await getPeers()
+  const newPeers = proposed.filter(x => !knownPeers.includes(x) && x !== address)
+  if (!newPeers.length) return false
 
-  const knownPeers = await readPeers()
-  proposed = [...proposed, ...knownPeers].filter(onlyUnique)
-  proposed = proposed.filter(x => x !== address)
-
-  fs.writeFile(file, proposed.join('\r\n'), (err) => {
-    if (err) throw err
-  })
+  const join = newPeers.join('\r\n')
+  const lines = knownPeers.length ? `\r\n${join}` : join
+  return appendLines(peerFile, lines)
 }
 
-module.exports = { readPeers, removePeer, tryAppendPeers }
+async function getLedger(fromHash = null) {
+  let blocks = await readLines(blockFile, true)
+
+  if (fromHash) {
+    const fromBlock = blocks.find(x => x.hash === fromHash)
+    blocks = blocks.slice(blocks.indexOf(fromBlock))
+  }
+
+  return blocks
+}
+
+async function tryAppendToLedger(proposed) {
+  const knownBlocks = await getLedger()
+  const newBlocks = proposed.filter(x => !knownBlocks.some(y => y.hash === x.hash))
+  if (!newBlocks.length) return false
+
+  const join = newBlocks.map(b => JSON.stringify(b)).join('\r\n')
+  const lines = knownBlocks.length ? `\r\n${join}` : join
+  return appendLines(blockFile, lines)
+}
+
+async function readLines(filePath, toObjects = false) {
+  const folder = filePath.split('/')[0]
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
+
+  const fileStream = fs.createReadStream(filePath, { flags: 'a+' });
+  const lineStream = readline.createInterface({ input: fileStream, crlfDelay: Infinity });
+
+  const lines = []
+  for await (const line of lineStream) {
+    lines.push(toObjects ? JSON.parse(line) : line)
+  }
+
+  return lines
+}
+
+function appendLines(filePath, lines) {
+  try {
+    fs.appendFileSync(filePath, lines, { flag: 'a+' })
+    console.log(`\r\nSuccessfully appended to ${filePath}\r\n${lines.trim()}.`)
+    return true
+  } catch {
+    return false
+  }
+}
+
+module.exports = { getPeers, removePeer, tryAppendPeers, getLedger, tryAppendToLedger }
